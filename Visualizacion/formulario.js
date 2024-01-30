85d1956b7a77f8ed58ed6961a2d4fb68d5c2b336
IsisTD <220126@fppiramide.com>
2024-01-30 20:48:31 +0100

    update

--- Visualizacion/formulario.js
+++ Visualizacion/formulario.js
@@ -2,12 +2,77 @@ const URL_SERVER ="http://3.234.32.136:3000/";
 
 document.addEventListener("DOMContentLoaded",()=>{
     cambiaUser(),
-    document.getElementById("")
+    cargarFlores()
+
 })
 
 function cambiaUser(e){
     const nombreUsuarioLS=JSON.parse(localStorage.getItem("users"));
     const user=document.getElementById("user");
      user.value=nombreUsuarioLS.nombre
+}
+
+function cargarFlores(e) {
+    fetch(`${URL_SERVER}plants/`)
+        .then(response => {
+            console.log(response); // Log the response details
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.status);
+            }
+        })
+        .then(data => {
+            console.log(data);
+            pintarFlores(data);
+        })
+        .catch(error => {
+            console.error("Error fetching plants:", error);
+            alert("No se ha podido traer las plantas");
+        });
+}
+
+
+function pintarFlores(flores){
+    flores.map(flor => {
+        const divContenido=document.getElementById("content");
+        const divFlor=document.createElement("div");
+        divFlor.classList.add("flores")
+        const divImagen=document.createElement("div");
+        divImagen.classList.add("image")
+        const imagen= document.createElement("img");
+        imagen.src=flor.imagen
+        imagen.classList.add("imagen")
+        const divTexto=document.createElement("div");
+        divTexto.classList.add("text")
+        const pNombre=document.createElement("p");
+        pNombre.classList.add("name")
+        pNombre.innerText=flor.nombre
+        const pPrecio=document.createElement("p");
+        pPrecio.classList.add("price")
+        pPrecio.innerText=flor.precio
+        const form=document.createElement("form");
+        const input=document.createElement("input");
+        input.type='submit'
+        input.value=`PURCHASE`;
+        input.classList.add("PURCHASE")
+        const divBasura=document.createElement("div");
+        divBasura.classList.add("basura")
+        const imagenBasura=document.createElement("img")
+        imagenBasura.src="https://s3.amazonaws.com/bucketactividad3.1/Basura.png"
+        
+
+        divBasura.append(imagenBasura);
+        form.append(input)
+        divTexto.append(pNombre,pPrecio)
+        divImagen.append(imagen)
+        divFlor.append(divImagen,divTexto,form,divBasura)
+        divContenido.append(divFlor)
+        
+    });
+
+    
+
+
 }
 
